8872d5e595a8455e0002c0e5ad7a92386a9ec212
Anurag Bhagsain <abhagsain@gmail.com>
2019-09-11 11:59:13 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="text-center my-10">
+      <h2 className="text-5xl">React With Tailwind</h2>
+    </div>
+  );
+}
+
+export default App;
