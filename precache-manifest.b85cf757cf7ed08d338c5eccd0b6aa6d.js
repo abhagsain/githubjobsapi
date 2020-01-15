c00ab65d4866fc728a0971f919a45d6660a67d24
Anurag Bhagsain <abhagsain@gmail.com>
2020-01-15 13:04:29 +0530

    add skeleton instead of loading indicatory [prod]

---
+++ precache-manifest.b85cf757cf7ed08d338c5eccd0b6aa6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ffd76b99f7f1de93b8b0a78944e083",
+    "url": "/githubjobsapi/index.html"
+  },
+  {
+    "revision": "60f340174b9ee51d10b7",
+    "url": "/githubjobsapi/static/css/main.55a5ac3a.chunk.css"
+  },
+  {
+    "revision": "1e77bf35ee87242b26bf",
+    "url": "/githubjobsapi/static/js/2.2ee3721c.chunk.js"
+  },
+  {
+    "revision": "60f340174b9ee51d10b7",
+    "url": "/githubjobsapi/static/js/main.1e3a7044.chunk.js"
+  },
+  {
+    "revision": "89113d0fb8811756cd6f",
+    "url": "/githubjobsapi/static/js/runtime~main.0d4a8828.js"
+  }
+]);
