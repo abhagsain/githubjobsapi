02cdb52f01ca359dffa7590411d521546391717b
Anurag Bhagsain <abhagsain@gmail.com>
2020-01-31 20:03:57 +0530

    Updates

---
+++ precache-manifest.93e7f024199b6494da3a2a596c96b7da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5ff0822b96cbe957630a2893f71faa",
+    "url": "/githubjobsapi/index.html"
+  },
+  {
+    "revision": "cb89fc73c513370dc04d",
+    "url": "/githubjobsapi/static/css/main.ba401202.chunk.css"
+  },
+  {
+    "revision": "1e77bf35ee87242b26bf",
+    "url": "/githubjobsapi/static/js/2.2ee3721c.chunk.js"
+  },
+  {
+    "revision": "cb89fc73c513370dc04d",
+    "url": "/githubjobsapi/static/js/main.73d8286e.chunk.js"
+  },
+  {
+    "revision": "89113d0fb8811756cd6f",
+    "url": "/githubjobsapi/static/js/runtime~main.0d4a8828.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8416f3996a930e5ad494c2d9483ad55c.js"
+  "/githubjobsapi/precache-manifest.93e7f024199b6494da3a2a596c96b7da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/githubjobsapi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
